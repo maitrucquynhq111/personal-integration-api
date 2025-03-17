082cc650b51995de1c6b06ed44a04503d3850dbd
maitrucquynh <97218479+quynhmido@users.noreply.github.com>
2025-03-17 21:41:35 +0700

    init api get lis database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const stripe = require("stripe")('sk_test_51QdA3qD5GjALdetb4ZZXb4STlsd9adOIlVUHqH6ya0c4F0d0SuJeaaOilobA0hTQgGLCtl1800mTSMiPfwwwcrFL00i3QFemAA');
 const dotenv = require("dotenv");
+const { Client } = require("@notionhq/client")
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
 
 dotenv.config();
 
@@ -13,41 +15,70 @@ app.use(cors());
 app.use(express.json());
 
 // API tạo Stripe Checkout Session
-app.post("/create-checkout-session", async (req, res) => {
-    try {
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            // line_items: [
-            //     {
-            //         price_data: {
-            //             currency: "usd",
-            //             product_data: {
-            //                 name: req.body.productName,
-            //             },
-            //             unit_amount: req.body.price * 100, // Giá sản phẩm (cents)
-            //         },
-            //         quantity: req.body.quantity,
-            //     },
-            // ],
-            mode: "subscription",
-            line_items: [
-                {
-                    price: "price_1QesqSD5GjALdetbBOBw3LaD",
-                    quantity: 1,
-                },
-            ],
-            client_reference_id: req.body?.affiliateId || "",
-            // allow_promotion_codes: req.body?.affiliateId ? false : true,
-            allow_promotion_codes: true,
-            success_url: `${req.headers.origin}/success`,
-            cancel_url: `${req.headers.origin}/cancel`,
-        });
+// app.post("/create-checkout-session", async (req, res) => {
+//     try {
+//         const session = await stripe.checkout.sessions.create({
+//             payment_method_types: ["card"],
+//             // line_items: [
+//             //     {
+//             //         price_data: {
+//             //             currency: "usd",
+//             //             product_data: {
+//             //                 name: req.body.productName,
+//             //             },
+//             //             unit_amount: req.body.price * 100, // Giá sản phẩm (cents)
+//             //         },
+//             //         quantity: req.body.quantity,
+//             //     },
+//             // ],
+//             mode: "subscription",
+//             line_items: [
+//                 {
+//                     price: "price_1QesqSD5GjALdetbBOBw3LaD",
+//                     quantity: 1,
+//                 },
+//             ],
+//             client_reference_id: req.body?.affiliateId || "",
+//             // allow_promotion_codes: req.body?.affiliateId ? false : true,
+//             allow_promotion_codes: true,
+//             success_url: `${req.headers.origin}/success`,
+//             cancel_url: `${req.headers.origin}/cancel`,
+//         });
 
-        res.json({ url: session.url });
+//         res.json({ url: session.url });
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+// Query database
+app.post("/databases/:databaseId", async function (request, response) {
+    const databaseId = request.params.databaseId;
+    const query = request.body
+
+    try {
+        const data = await notion.databases.query({
+            database_id: databaseId,
+            query: query
+        })
+        response.json({ message: "success!", data: data })
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        response.json({ message: "error", error })
     }
-});
+})
+
+// API tạo page
+app.post("/page", async function (request, response) {
+    const query = request.body
+    console.log("🚀 ~ query:", query)
+
+    try {
+        const data = await notion.pages.create(query)
+        response.json({ message: "success!", data: data })
+    } catch (error) {
+        response.json({ message: "error", error })
+    }
+})
 
 // Chạy server
 const PORT = process.env.PORT || 4242;
